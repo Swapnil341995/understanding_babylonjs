521f335cc9ff57c725e0012fd5bc74c8e6cb6bcf
Swapnil341995 <sbharude.works@gmail.com>
2022-09-04 14:43:52 +0530

    transformations related to scale, position and rotation in new folder structure

--- babylon-2/js/app.js
+++ babylon-2/js/app.js
@@ -7,20 +7,19 @@ let ADD = 0.01,
   theta = 0;
 //scene set up
 const createScene = () => {
-  const bblon = BABYLON;
   const canvasElement = document.getElementById("canvas_id");
   canvasElement.height = window.innerHeight;
   canvasElement.width = window.innerWidth;
-  viewer.engine = new bblon.Engine(canvasElement, true);
-  viewer.scene = new bblon.Scene(viewer.engine);
+  viewer.engine = new BABYLON.Engine(canvasElement, true);
+  viewer.scene = new BABYLON.Scene(viewer.engine);
 
   //adding cube
-  const cube = bblon.MeshBuilder.CreateBox("cube", viewer.scene);
-  cube.material = new bblon.StandardMaterial("cube_material", viewer.scene);
-  cube.material.diffuseColor = bblon.Color3.Red();
+  const cube = BABYLON.MeshBuilder.CreateBox("cube", viewer.scene);
+  cube.material = new BABYLON.StandardMaterial("cube_material", viewer.scene);
+  cube.material.diffuseColor = BABYLON.Color3.Red();
 
   //adding ground plane
-  const ground = bblon.MeshBuilder.CreatePlane(
+  const ground = BABYLON.MeshBuilder.CreatePlane(
     "ground",
     {
       height: 2,
@@ -30,24 +29,41 @@ const createScene = () => {
     viewer.scene
   );
   ground.position.z = 1;
-  ground.material = new bblon.StandardMaterial("ground_material", viewer.scene);
+  ground.material = new BABYLON.StandardMaterial(
+    "ground_material",
+    viewer.scene
+  );
 
   //adding camera
-  const camera = new bblon.ArcRotateCamera(
+  const camera = new BABYLON.ArcRotateCamera(
     "camera",
     -Math.PI / 2,
     Math.PI / 2.5,
     15,
-    new bblon.Vector3(0, 0, 0)
+    new BABYLON.Vector3(0, 0, 0)
   );
   camera.attachControl(canvasElement, true);
 
   //adding light
-  const light = new bblon.HemisphericLight(
+  const light = new BABYLON.HemisphericLight(
     "light",
-    new bblon.Vector3(1, 1, 0),
+    new BABYLON.Vector3(1, 1, 0),
     viewer.scene
   );
+};
+
+//change scale position and rotation all at the same time.
+const changeScalePositionRotation = () => {
+  const ground = viewer.scene.getMeshByName("ground");
+  ground.position.x = 2 * Math.sin(theta);
+  ground.position.y = 2 * Math.cos(theta);
+
+  ground.scaling.x = 2 * Math.sin(theta);
+  ground.scaling.y = 2 * Math.cos(theta);
+
+  ground.rotation.x = 2 * Math.sin(theta);
+  ground.rotation.y = 2 * Math.cos(theta);
+  theta += ADD;
 };
 
 //handling events
@@ -67,4 +83,5 @@ addEventListeners();
 //game engine render loop
 viewer.engine.runRenderLoop(() => {
   viewer.scene.render();
+  changeScalePositionRotation();
 });
